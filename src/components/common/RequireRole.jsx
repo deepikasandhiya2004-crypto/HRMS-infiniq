c36504ee941171bf77b409e3276ab7022bd8676b
deepikasandhiya2004-crypto <deepikasandhiya2004@gmail.com>
2026-09-21 15:57:38 +0530

    Clear old project content

--- src/components/common/RequireRole.jsx
+++
@@ -1,6 +0,0 @@
-import { hasRole } from "../../lib/auth.js";
-
-export default function RequireRole({ roles, children, fallback = null }) {
-  if (!hasRole(...roles)) return fallback;
-  return children;
-}
--- src/services/mockData.js
+++
@@ -1,4 +0,0 @@
-﻿export * from "./mockLeave.js";
-export * from "./mockEmployees.js";
-export * from "./mockOperations.js";
-export * from "./mockConfig.js";
